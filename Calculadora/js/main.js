eb2ddc8152004b319df6441f9df453b5f68758e1
David Diaz <85139022+DavidDipi@users.noreply.github.com>
2023-10-13 11:53:15 -0500

    Update main.js

--- Calculadora/js/main.js
+++ Calculadora/js/main.js
@@ -1,44 +1,43 @@
 const pantalla = document.querySelector(".pantalla")
 const botones = document.querySelectorAll(".boton")
 
+// Itera a través de cada elemento en la lista de botones y agrega un evento de click a cada uno.
 botones.forEach(boton => {
     boton.addEventListener("click", ()=>{
+        // Obtiene el texto del botón actual
         const botonOn = boton.textContent
 
-        if (boton.id === "borrar") {
-            if (pantalla.textContent.length === 1) {
+        if ( boton.id === "borrar" ) {
+            if ( pantalla.textContent.length === 1 ) {
                 pantalla.textContent = "0";
-            }else{
-                pantalla.textContent = pantalla.textContent.slice(0,-1);
-               
-             }
-              return;
-            
+            } else{
+                pantalla.textContent = pantalla.textContent.slice(0,-1); 
             }
+            return;
+            
+        }
 
-        if (boton.id === "igual") {
+        if ( boton.id === "igual" ) {
             try{
                 pantalla.textContent = eval(pantalla.textContent)
-                if (pantalla.textContent === "") {  
+                if ( pantalla.textContent === "" ) {  
                     pantalla.textContent = "¡Error!"
                 }
             }catch{
                 pantalla.textContent = "¡Error!"
-             }
-             return
+            }
+            return;
         }
 
-       
-        
-         if (boton.id === "limpiar") {
-             pantalla.textContent = "0";
-              return;
-            }
+        if ( boton.id === "limpiar" ) {
+            pantalla.textContent = "0";
+            return;
+        }
 
-            if (pantalla.textContent === "0" || pantalla.textContent === "¡Error!") {
-                pantalla.textContent = botonOn
-            }else{
-                pantalla.textContent += botonOn
-            }        
+        if (pantalla.textContent === "0" || pantalla.textContent === "¡Error!") {
+            pantalla.textContent = botonOn
+        }else{
+            pantalla.textContent += botonOn
+        }        
     })
 });
